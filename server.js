ed3b971a0e10c5be2e222e1b71ec17b718a34bc3
Alicia Glenn <alicia@aliciaglenn.com>
2022-11-30 08:21:08 -0500

    created .env file, connected mongodb, created .gitingnore file and imported dependenciesa to server.js

---
+++ server.js
@@ -0,0 +1,27 @@
+/////////////////////////////////////////////
+// Import Our Dependencies
+/////////////////////////////////////////////
+require("dotenv").config(); // Load ENV Variables
+const express = require("express"); // import express
+const morgan = require("morgan"); //import morgan
+const methodOverride = require("method-override");
+const mongoose = require("mongoose");
+
+/////////////////////////////////////////////
+// Database Connection
+/////////////////////////////////////////////
+// Setup inputs for our connect function
+const DATABASE_URL = process.env.DATABASE_URL;
+const CONFIG = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+// Establish Connection
+mongoose.connect(DATABASE_URL, CONFIG);
+
+// Events for when connection opens/disconnects/errors
+mongoose.connection
+  .on("open", () => console.log("Connected to Mongoose"))
+  .on("close", () => console.log("Disconnected from Mongoose"))
+  .on("error", (error) => console.log(error));
